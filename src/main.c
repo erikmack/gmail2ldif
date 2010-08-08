adf3113c9316c6d56b473e8794d1bd37e631efc7
Erik Mackdanz <erikmack@gmail.com>
2010-08-08 12:16:47 -0500

    Add copyright/license notice to source files

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2010 Free Software Foundation
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -60,8 +77,6 @@ static void show_usage() {
 
 	static const char * usage = 
 		"\n"
-		PACKAGE_NAME " version " PACKAGE_VERSION "\n"
-		"\n"
 		"Convert a contact list exported from GMail (in Google-format .csv) to\n"
 		"LDIF, ready for import to an LDAP server.\n"
 		"\n"
@@ -80,6 +95,14 @@ static void show_usage() {
 		"\n"
 		"\n"
 		"See man " PACKAGE_NAME " for more information.\n"
+		"\n"
+		PACKAGE_NAME " version " PACKAGE_VERSION "\n"
+		"Copyright (C) 2010 Free Software Foundation\n"
+		"\n"
+		"This program comes with ABSOLUTELY NO WARRANTY.\n"
+		"This is free software, and you are welcome to redistribute it\n"
+		"under certain conditions; visit http://www.gnu.org/licenses/gpl.htm\n"
+		"for details.\n"
 	;
 	
 	fprintf( stderr, "%s\n", usage );
